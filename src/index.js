ed0f4bc5f4e91781824fc62a3c614788a5226f86
Lahavh <lahav2005@hotmail.com>
2021-10-31 00:22:15 +0300

    backend server update and database fetch posts and users

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
 const mongodbURL = "mongodb://localhost:27017";
 const mongodbName = "makeup-community";
-const mongodbCollectionName = "users";
+const mongodbUsersCollectionName = "users";
+const mongodbPostsCollectionName = "posts";
 
 const port = "5555";
 
@@ -28,19 +29,40 @@ app.get("/", (request, response) => {
 app.get("/users", (request, response) => {
     mongoClient.connect(mongodbURL, (error, mongo) => {
         if (error) {
-            console.log(error);
-            return;
+            return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
         }
 
-        mongo.db(mongodbName).collection(mongodbCollectionName).find({}).toArray((error, users) => {
+        mongo.db(mongodbName).collection(mongodbUsersCollectionName).find({}).toArray((error, users) => {
             if (error) {
-                console.log(error);
-                return;
+                return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
             }
 
             mongo.close();
             users = users || [];
             response.send(users);
+        });
+    });
+});
+
+
+app.get("/user/::email/::password", (request, response) => {
+    mongoClient.connect(mongodbURL, (error, mongo) => {
+        if (error) {
+            return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+
+        const query = {
+            email: request.params.email,
+            password: request.params.password
+        };
+
+        mongo.db(mongodbName).collection(mongodbUsersCollectionName).findOne(query, { projection: {"_id": false} }, (error, user) => {
+            if (error) {
+                return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+            }
+
+            mongo.close();
+            response.send(user);
         });
     });
 });
@@ -52,13 +74,68 @@ app.post("/user", jsonParser, (request, response)=> {
             return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
         }
 
-        mongo.db(mongodbName).collection(mongodbCollectionName).insertOne(request.body, (error) => {
+        mongo.db(mongodbName).collection(mongodbUsersCollectionName).insertOne(request.body, (error) => {
             if(error) {
                 return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
             }
 
             mongo.close();
             response.send(request.body);
+        });
+    });
+});
+
+app.post("/post", jsonParser, (request, response)=> {
+    mongoClient.connect(mongodbURL, (error, mongo)=> {
+        if(error) {
+            return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+
+        mongo.db(mongodbName).collection(mongodbPostsCollectionName).insertOne(request.body, (error) => {
+            if(error) {
+                return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+            }
+
+            mongo.close();
+            response.send(request.body);
+        });
+    });
+});
+
+app.put("/post", jsonParser, (request, response)=> {
+    mongoClient.connect(mongodbURL, (error, mongo)=> {
+        if(error) {
+            return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+
+        const query = { id: request.body.authorId };
+        const update = { $push: { myPosts: request.body } };
+
+        mongo.db(mongodbName).collection(mongodbUsersCollectionName).updateOne(query, update, (error) => {
+            if(error) {
+                return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+            }
+
+            mongo.close();
+            response.send(request.body);
+        });
+    });
+});
+
+app.get("/posts", (request, response) => {
+    mongoClient.connect(mongodbURL, (error, mongo) => {
+        if (error) {
+            return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+
+        mongo.db(mongodbName).collection(mongodbPostsCollectionName).find({}).toArray((error, posts) => {
+            if (error) {
+                return response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+            }
+
+            mongo.close();
+            posts = posts || [];
+            response.send(posts);
         });
     });
 });
